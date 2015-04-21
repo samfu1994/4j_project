6c1f6e57a5af951334c4a5c0aeb4fd0dc82cda2b
DaiLing <qzyz_dailing@163.com>
2015-04-21 19:34:41 +0800

    finished train part, add predict next time

--- prob.cpp
+++ prob.cpp
@@ -31,7 +31,7 @@ using namespace std;
             &((f).value)))
 
 #define PRINT_FEATURE(f)\
-        printf("%-7d %.15lf\n",f.index, f.value)
+        printf("%-7d %.15lf\n",(f).index, (f).value)
 
 /* data structure defination */
 struct lable_node{
@@ -41,11 +41,28 @@ struct lable_node{
     int  group1;
     int  group2;
 };
+struct trainThreadParams{
+    int         groupNum;
+    problem*    prob;
+    parameter*  param;
+    model**     retData;
+    trainThreadParams(int _gn, \
+        problem* _prob, \
+        parameter* _param, \
+        model** _retData )
+    {
+        groupNum = _gn;
+        prob = _prob;
+        param = _param;
+        retData = _retData;
+    }
+};
 
 /* const defination */
 const int NUM_FEATURE = 5001;
-const int NUM_POSITIVE = 8;
-const int NUM_NEGATIVE = 24;
+const int NUM_POSITIVE = 2;
+const int NUM_NEGATIVE = 4;
+const int NUM_GROUP    = NUM_NEGATIVE*NUM_POSITIVE;
 const double BIAS = 1;
 const feature_node endOfFeature = {-1,0};
 const feature_node biasFeature = {NUM_FEATURE,BIAS};
@@ -62,27 +79,78 @@ int readData(const char * fileName, \
         vector< vector<feature_node> >&features);
 int getTargetVal(vector<vector<lable_node> > & labs, \
         vector<double>& retVal);
-
 int classify(vector< vector<lable_node> > &lables, \
         vector< vector<feature_node> >&features, \
-        vector< std::vector<feature_node* > > & retFeature , \
-        std::vector<std::vector<double> > &retTargetval);
+        vector< vector<feature_node* > > & retFeature , \
+        vector<vector<double> > &retTargetval);
+int getGroupParam(vector< vector<feature_node* > > &gFeature , \
+        vector<vector<double> > &gTargetval , \
+        vector<parameter> &retParam, \
+        vector<problem>  &retProb \
+);
+void * trainThreadFunc(void *);
 
 /* functions */
 int main(){
-    std::vector<std::vector<double> >     gTargetval;
-    
+    vector<vector<double> >             gTargetval;
+    vector< vector<feature_node* > >    gFeature;
+    vector<parameter>                   gParam;
+    vector<problem>                     gProb;
+    vector<model*>                      gmodel;
+    threads poll(8);
+
+    // read data
     int N = readData("data/train.txt",lables,features);
     getTargetVal(lables,targetVal);
-    vector< std::vector<feature_node* > > gFeature;
     classify(lables,features,gFeature,gTargetval);
-    printf("Finished classi\n");
+    printf("Finished classify\n");
 
-    threads poll(8);
+    // predict
+    getGroupParam(gFeature,gTargetval,gParam,gProb);
+    gmodel.reserve(NUM_GROUP);
+    for(int i = 0; i < NUM_GROUP; i++){
+        poll.addJob(trainThreadFunc,new trainThreadParams(i,&gProb[i],&gParam[i],&gmodel[i]));
+    }
+
     poll.stop();
     return 0;
 }
-
+void * trainThreadFunc(void * p){
+    trainThreadParams * pa = (trainThreadParams*)p;
+    *(pa->retData) = train(pa->prob,pa->param);
+    return NULL;
+}
+int getGroupParam(vector< vector<feature_node* > > &gFeature , \
+        vector<vector<double> > &gTargetval , \
+        vector<parameter> &retParam, \
+        vector<problem>  &retProb \
+)
+{
+    retParam.clear();
+    retProb.clear();
+    retParam.reserve(NUM_GROUP);
+    retProb.reserve(NUM_GROUP);
+    for(int i = 0; i < NUM_GROUP; i++){
+        retProb[i].l = gFeature[i].size();
+        retProb[i].n = NUM_FEATURE;
+        retProb[i].y = gTargetval[i].data();
+        retProb[i].x = gFeature[i].data();
+        retProb[i].bias = 1;
+        retParam[i].solver_type = L2R_L2LOSS_SVC_DUAL;
+        retParam[i].C = 1;
+        retParam[i].eps = 0.1;
+        retParam[i].p = 0.1;
+        retParam[i].nr_weight = 0;
+        retParam[i].weight = NULL;
+        retParam[i].weight_label = NULL;
+        const char * c = check_parameter(&retProb[i],&retParam[i]);
+        if(c){
+            printf("%s\n", c);
+            return -1;
+        }
+    }
+    return 0;
+}
 
 int readData(const char * fileName, \
         vector< vector<lable_node> > &lables, \
@@ -96,8 +164,10 @@ int readData(const char * fileName, \
     while(feof(fin) == 0){
         lables.push_back(vector<lable_node>());
         features.push_back(vector<feature_node>());
-        while(SCAN_LABLE(fin,tlable)) lables[N].push_back(tlable);
-        while(SCAN_FEATURE(fin,tfeature) && feof(fin) == 0) features[N].push_back(tfeature);
+        while(SCAN_LABLE(fin,tlable))
+            lables[N].push_back(tlable);
+        while(SCAN_FEATURE(fin,tfeature) && feof(fin) == 0)
+            features[N].push_back(tfeature);
         if(BIAS >= 0) features[N].push_back(biasFeature);
         features[N].push_back(endOfFeature);
         N++;
@@ -130,32 +200,34 @@ int getTargetVal(vector<vector<lable_node> > & labs, \
 */
 int classify(vector< vector<lable_node> > &lables, \
         vector< vector<feature_node> >&features, \
-        vector< std::vector<feature_node* > > & retFeature , \
-        std::vector<std::vector<double> > &retTargetval)
+        vector< vector<feature_node* > > & retFeature , \
+        vector<vector<double> > &retTargetval)
 {
     int counterP = 0;
     int counterN = 0;
-    int numGroup = NUM_POSITIVE*NUM_NEGATIVE;
-    // count possitives
     retFeature.clear();
     retTargetval.clear();
-    for(int i = 0; i < numGroup; i++){
-        retTargetval.push_back(std::vector<double> ());
-        retFeature.push_back(std::vector<feature_node*>());
+    // create data streucture
+    for(int i = 0; i < NUM_GROUP; i++){
+        retTargetval.push_back(vector<double> ());
+        retFeature.push_back(vector<feature_node*>());
     }
+    // classify data
     for (unsigned int i = 0; i < features.size(); ++i){
         if(lables[i][0].Section == 'A'){
-
-
-            retFeature[counterP].push_back(features[i].data());
-            retTargetval[counterP].push_back(1);
+            for(int j = counterP; j < NUM_GROUP; j+=NUM_POSITIVE){
+                retFeature[j].push_back(features[i].data());
+                retTargetval[j].push_back(1);
+            }
             counterP++;
-            counterP = counterP % numGroup;
+            counterP = counterP % NUM_POSITIVE;
         }else{
-            retFeature[counterN].push_back(features[i].data());
-            retTargetval[counterN].push_back(-1);
+            for(int j = counterN*NUM_POSITIVE; j < counterN*NUM_POSITIVE+NUM_POSITIVE; j++){
+                retFeature[j].push_back(features[i].data());
+                retTargetval[j].push_back(-1);
+            }
             counterN++;
-            counterN = counterN % numGroup;
+            counterN = counterN % NUM_NEGATIVE;
         }
     }
     return 0;
